7bd449aab593e7578e06d3d002b5c447798cdcd3
thakurbhanu021 <thakurbhanu021@gmail.com>
2023-08-03 13:06:29 +0530

    notification file setup

--- src/App.js
+++ src/App.js
@@ -3,9 +3,18 @@ import { useSelector } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import { useEffect } from 'react';
 
 function App() {
-  const cartShow = useSelector(state=> state.ui.cartIsVisible)
+  const cartShow = useSelector(state=> state.ui.cartIsVisible);
+  const cart = useSelector(state=> state.cart);
+
+  useEffect(()=>{
+    fetch('https://food-order-app-react-6fc17-default-rtdb.firebaseio.com/cart.json',{
+      method: 'PUT',
+      body: JSON.stringify(cart),
+    })
+  },[cart])
   return (
     <Layout>
       {cartShow && <Cart />}
